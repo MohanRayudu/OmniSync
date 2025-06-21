b3d767f8a02adf8c8b2bcf0123b3c3a5164a846c
MohanRayudu <55494517+MohanRayudu@users.noreply.github.com>
2025-06-21 16:33:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.get('/stream', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('✅ Server running at http://localhost:3000');
+  console.log('✅ Server running at http://https://omnisynclogin.onrender.com:3000');
 });
